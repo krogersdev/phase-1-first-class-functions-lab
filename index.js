91f911143b8498f985e17679b15da22708eca2a2
Kevin Rogers <kevinarogers1@gmail.com>
2021-11-24 19:47:21 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 
 function returnFirstTwoDrivers(driverArray) {
-    const firstTwo = driverArray.slice(0, 2)
+    const firstTwo = driverArray.slice(0, 2);
     return firstTwo;
 }
 
 function returnLastTwoDrivers(driverArray) {
-    const lastTwo = driverArray.slice(-2)
+    const lastTwo = driverArray.slice(-2);
     return lastTwo;
 }
 
 let selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
 
 function createFareMultiplier(multiplier) {
-    return (fare) => multiplier * fare;
+    return (fare) => multiplier * fare
 }
 
 const fareDoubler = createFareMultiplier(2);
 
 const fareTripler = createFareMultiplier(3);
 
-function selectDifferentDrivers(arrayOfDrivers, func) {
-    return func(arrayOfDrivers);
+function selectDifferentDrivers(array, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(array);
 }
