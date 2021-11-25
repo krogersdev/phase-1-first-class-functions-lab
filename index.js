108116a85cce08bb1db4f59e228071df35fa845d
Kevin Rogers <kevinarogers1@gmail.com>
2021-11-24 19:41:51 -0500

    commit tree 00740ca205773af232f9b4283508e6f437435621

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+
+function returnFirstTwoDrivers(driverArray) {
+    const firstTwo = driverArray.slice(0, 2)
+    return firstTwo;
+}
+
+function returnLastTwoDrivers(driverArray) {
+    const lastTwo = driverArray.slice(-2)
+    return lastTwo;
+}
+
+let selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return (fare) => multiplier * fare;
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, func) {
+    return func(arrayOfDrivers);
+}
